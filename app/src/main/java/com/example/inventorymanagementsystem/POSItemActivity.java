b4a016f8a967c5dd49e054000afbba0661d21c5f
Alxbry28 <46429925+Alxbry28@users.noreply.github.com>
2022-11-20 04:13:28 +0800

    [staff][cart] created cart activity and library

--- app/src/main/java/com/example/inventorymanagementsystem/POSItemActivity.java
+++ app/src/main/java/com/example/inventorymanagementsystem/POSItemActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
@@ -15,6 +16,7 @@ import android.widget.Toast;
 import com.example.inventorymanagementsystem.adapters.POSRCVAdapter;
 import com.example.inventorymanagementsystem.databinding.FragmentSecondBinding;
 import com.example.inventorymanagementsystem.interfaces.ProductModelListener;
+import com.example.inventorymanagementsystem.libraries.CartLibrary;
 import com.example.inventorymanagementsystem.models.Product;
 
 import java.util.ArrayList;
@@ -22,14 +24,15 @@ import java.util.ArrayList;
 public class POSItemActivity extends AppCompatActivity {
 
     private SharedPreferences sharedPreferences;
-
+    private String businessName, storeId, userId;
     private RecyclerView rcPOSProductItem;
     private TextView tvPOSItemMsg;
     private Product product;
     private ArrayList<Product> productList;
     private ArrayList<Product> productSelectedList;
-    private String businessName, storeId, userId;
-    private Button btnCheckout;
+
+    private Button btnCheckout,btnCart;
+    private CartLibrary cartLibrary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +40,10 @@ public class POSItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_positem);
         productSelectedList = new ArrayList<>();
         product = new Product();
+        cartLibrary = new CartLibrary();
+
         sharedPreferences = getSharedPreferences(MainActivity.TAG, Context.MODE_PRIVATE);
+
         businessName = sharedPreferences.getString("businessName",null);
         storeId = sharedPreferences.getString("storeId",null);
         userId = sharedPreferences.getString("userId",null);
@@ -45,6 +51,7 @@ public class POSItemActivity extends AppCompatActivity {
         product.setStoreId(storeId);
 
         btnCheckout = findViewById(R.id.btnCheckout);
+        btnCart = findViewById(R.id.btnCart);
         rcPOSProductItem = findViewById(R.id.rcPOSProductItem);
         POSRCVAdapter posRCVAdapter = new POSRCVAdapter();
         product.GetAll(new ProductModelListener() {
@@ -77,8 +84,15 @@ public class POSItemActivity extends AppCompatActivity {
             @Override
             public void getSelectedItem(Product product) {
                 productSelectedList.add(product);
+                cartLibrary.setProductArrayList(productSelectedList);
+
                 btnCheckout.setText(productSelectedList.size() + " Items");
             }
+        });
+
+        btnCart.setOnClickListener(v->{
+            startActivity(new Intent(POSItemActivity.this, CartActivity.class));
+//            finish();
         });
 
     }
