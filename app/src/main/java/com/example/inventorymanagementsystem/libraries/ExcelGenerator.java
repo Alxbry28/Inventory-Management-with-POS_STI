d3a1ac3ff9be4ad40eff279e5f018d3b20f40db3
Alxbry28 <46429925+Alxbry28@users.noreply.github.com>
2023-01-06 15:58:11 +0800

    [fix4]

--- app/src/main/java/com/example/inventorymanagementsystem/libraries/ExcelGenerator.java
+++ app/src/main/java/com/example/inventorymanagementsystem/libraries/ExcelGenerator.java
@@ -73,7 +73,6 @@ public class ExcelGenerator {
         File filePathBackup = new File(folderDocumentBackup, fileNameUnique+"_"+Sales.FILENAME);
         try {
 
-
             if(!folderDocument.exists()){
                 folderDocument.mkdirs();
             }
@@ -83,9 +82,7 @@ public class ExcelGenerator {
             }
 
             if (filePath.exists()){
-
                 filePath.delete();
-
 
                 if (!filePathBackup.exists()){
                     filePathBackup.createNewFile();
