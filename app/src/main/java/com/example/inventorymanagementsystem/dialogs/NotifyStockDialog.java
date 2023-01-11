9413583413674b133bac3fcb88aeb7e03ff61f60
Alxbry28 <46429925+Alxbry28@users.noreply.github.com>
2023-01-11 12:48:08 +0800

    [revision] created dialog notification

--- app/src/main/java/com/example/inventorymanagementsystem/dialogs/NotifyStockDialog.java
+++ app/src/main/java/com/example/inventorymanagementsystem/dialogs/NotifyStockDialog.java
@@ -3,6 +3,7 @@ package com.example.inventorymanagementsystem.dialogs;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -15,12 +16,13 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
 import com.example.inventorymanagementsystem.R;
+import com.example.inventorymanagementsystem.enums.StockNotification;
 
 public class NotifyStockDialog extends AppCompatDialogFragment {
 
     private Context context;
     private String message;
-    private int resourceImage;
+    private StockNotification stockNotification;
 
     public NotifyStockDialog(Context context){
         this.context = context;
@@ -30,12 +32,51 @@ public class NotifyStockDialog extends AppCompatDialogFragment {
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Stock Notification");
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.dialog_notify_product_stocks,null);
+
         ImageView ivNotifImage = view.findViewById(R.id.ivNotifImage);
         TextView tvMessage = view.findViewById(R.id.tvMessage);
-        ivNotifImage.setBackgroundResource();
+
+        if(message.isEmpty() || message == null){
+            tvMessage.setText(stockNotification.Message);
+        }
+        else{
+            tvMessage.setText(message);
+        }
+
+
+        ivNotifImage.setBackgroundResource(stockNotification.Icon);
+
+
+
+
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                dialog.dismiss();
+            }
+        });
+        builder.setView(view);
 
         return builder.create();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public StockNotification getStockNotification() {
+        return stockNotification;
+    }
+
+    public void setStockNotification(StockNotification stockNotification) {
+        this.stockNotification = stockNotification;
     }
 }
--- app/src/main/java/com/example/inventorymanagementsystem/views/staff/InventoryForm.java
+++ app/src/main/java/com/example/inventorymanagementsystem/views/staff/InventoryForm.java
@@ -34,6 +34,7 @@ public class InventoryForm extends AppCompatActivity {
     private Product product;
     private ArrayList<Product> productList;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
